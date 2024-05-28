bd87234c77cc5acfdecdab6aaaf175caa55113d5
JoaniPran <jpranteda@fi.uba.ar>
2024-05-27 23:21:00 -0300

    archivos modificados

--- asmlab/main.c
+++ asmlab/main.c
@@ -8,24 +8,7 @@
 #include "lib.h"
 
 int main (void){    
-    int32_t number = 12;
-    char str[] = "espada";
-    card_t *card = cardNew(str, &number);
-
-    int32_t number2 = 4;
-    char str2[] = "oro";
-    card_t *card2 = cardNew(str2, &number2);
-
-    cardAddStacked(card, card2);
-
-    card_t *cardStacked = listGet(card->stacked, 0);
-
-    assert((card->stacked->size) == 1);
-    assert(*(cardStacked->number) == number2);
-    assert(strcmp(cardStacked->suit, str2) == 0);
-
-    cardDelete(card);
-    cardDelete(card2);
+    return 0;
 }
 
 
