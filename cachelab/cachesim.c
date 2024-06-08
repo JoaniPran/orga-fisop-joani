0e723ff24a34185d3875c1adbb0811ee8c8c4b32
Gonzalo <llaos@fi.uba.ar>
2024-06-08 19:21:22 -0300

    Primeras comprobaciones

--- cachelab/cachesim.c
+++ cachelab/cachesim.c
@@ -4,9 +4,38 @@
 #include <string.h>
 #include <assert.h>
 #include <math.h>
+#include <getopt.h>
 
-int main (void){    
+void argsParse(int args, char* argv[]);
+
+int main (int argc, char* argv[]){ 
+    argsParse(argc, argv);   
     return 0;
+}
+
+void argsParse(int argc, char* argv[]) {
+    if(argc != 5 && argc != 8) {
+        fprintf(stderr, "La cantidad de parámetros no es la correcta.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    char* archivoTraza = malloc(strlen(argv[1]) + 
+    strlen("trazas/") + 1);
+    strcpy(archivoTraza, "trazas/");
+    strcat(archivoTraza, argv[1]);
+    
+    FILE *file = fopen(archivoTraza, "r");
+    
+    if(file == NULL) {
+        fprintf(stderr, "Archivo inexistente.\n");
+        exit(EXIT_FAILURE);
+    }
+    else {
+        printf("%s", argv[1]);
+        printf("\n");
+    }
+
+    fclose(file);
 }
 
 
